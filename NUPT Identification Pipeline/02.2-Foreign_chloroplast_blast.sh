95f8d9a1133a255845038936b756b87f560303e9
Noah Bourne <91956837+NBourne04@users.noreply.github.com>
2026-07-02 15:15:07 +0100

    Create 02.2-Foreign_chloroplast_blast.sh
    Blast all of the foreign chloroplast against the target species nuclear genomes.

---
+++ NUPT Identification Pipeline/02.2-Foreign_chloroplast_blast.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Activate conda environment
+source /path/to/miniconda3/bin/activate
+source activate /path/to/miniconda3/envs/blast
+
+# List of species
+species_list=("AUS1_2019" "RSA5-3" "TAN1-04B" "ZAM1505-10")
+
+for species in "${species_list[@]}"; do
+    echo "Running BLAST for $species..."
+
+blastn -query Chloroplasts/All_chloroplasts_no_ASEM.fasta \
+               -db "BlastDB/${species}/${species}" \
+               -out "Blast_outs/outgroups/All_chloroplasts_no_ASEM_vs_${species}.out" \
+               -evalue 1e-10 \
+               -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore sseq" \
+               -num_threads 1
+
+done
